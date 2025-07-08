bfba4c18d720ebe11458aa105d2b600a0025eb4c
Caroline Bohadana Rodrigues Viana <232050975@aluno.unb.br>
2025-07-08 19:24:49 -0300

    Update criacao_tabelas.sql

--- criacao_tabelas.sql
+++ criacao_tabelas.sql
@@ -1,3 +1,14 @@
+/*
+
+
+Aluna: Caroline Bohadana Rodrigues Viana
+Matrícula: 232050975
+
+Trabalho Final de Banco de Dados
+
+
+*/
+
 CREATE TABLE Departamento(
     Codigo VARCHAR(3) PRIMARY KEY,
     Nome VARCHAR(50) NOT NULL
@@ -6,18 +17,20 @@ CREATE TABLE Departamento(
 CREATE TABLE Curso(
     Codigo INTEGER PRIMARY KEY,
     Nome VARCHAR(50) NOT NULL,
-    Tipo VARCHAR(30) NOT NULL,
-    Cod_Departamento VARCHAR(3),
-    FOREIGN KEY (Cod_Departamento) REFERENCES Departamento(Codigo)
+	Tipo VARCHAR(30) NOT NULL,
+	Cod_Departamento VARCHAR(3),
+	FOREIGN KEY (Cod_Departamento) REFERENCES Departamento(Codigo)
 );
 
 CREATE TABLE Participante(
     CPF VARCHAR(11) PRIMARY KEY,
     Nome VARCHAR(50) NOT NULL,
-    Ocupacao VARCHAR(40) NOT NULL,
-    Email VARCHAR(30),
-    Cod_curso INTEGER,
-    FOREIGN KEY (Cod_curso) REFERENCES Curso(Codigo)
+	Ocupacao VARCHAR(40) NOT NULL,
+	Email VARCHAR(30),
+	Cod_curso INTEGER,
+	Foto BYTEA,
+	FotoCadastrada BOOLEAN DEFAULT FALSE,
+	FOREIGN KEY (Cod_curso) REFERENCES Curso(Codigo)
 );
 
 CREATE TABLE Telefone(
@@ -29,62 +42,62 @@ CREATE TABLE Telefone(
 
 CREATE TABLE TipoAtividade(
     Codigo INTEGER PRIMARY KEY,
-    Nome VARCHAR(50) NOT NULL
+	Nome VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE LocalAtividade(
     Codigo INTEGER PRIMARY KEY,
     Nome VARCHAR(180) NOT NULL,
-    Sigla VARCHAR(3) NOT NULL,
-    Capacidade INTEGER
+	Sigla VARCHAR(3) NOT NULL,
+	Capacidade INTEGER
 );
 
 CREATE TABLE Atividade(
     Codigo INTEGER PRIMARY KEY,
-    Tema VARCHAR(60) NOT NULL,
-    Cod_TipoAti INTEGER,
+	Tema VARCHAR(60) NOT NULL,
+	Cod_TipoAti INTEGER,
     Descricao TEXT,
-    Programacao TEXT,
-    DataInicio DATE,
-    DataFim DATE,
-    HorarioInicio TIME,
-    HorarioFim TIME,
-    CargaHoraria INTEGER,
-    Vagas INTEGER,
-    Cod_Local INTEGER,
-    FOREIGN KEY (Cod_TipoAti) REFERENCES TipoAtividade(Codigo),
+	Programacao TEXT,
+	DataInicio DATE,
+	DataFim DATE,
+	HorarioInicio TIME,
+	HorarioFim TIME,
+	CargaHoraria INTEGER,
+	Vagas INTEGER,
+	Cod_Local INTEGER,
+	FOREIGN KEY (Cod_TipoAti) REFERENCES TipoAtividade(Codigo),
     FOREIGN KEY (Cod_Local) REFERENCES LocalAtividade(Codigo)
 );
 
 CREATE TABLE Inscricao(
     Codigo INTEGER PRIMARY KEY,
     Cod_Atividade INTEGER,
-    CPF_Participante VARCHAR(11),
-    DataIncricao DATE,
-    Presenca BOOLEAN NOT NULL,
-    FOREIGN KEY (Cod_Atividade) REFERENCES Atividade(Codigo),
+	CPF_Participante VARCHAR(11),
+	DataInscricao DATE,
+	Presenca BOOLEAN NOT NULL,
+	FOREIGN KEY (Cod_Atividade) REFERENCES Atividade(Codigo),
     FOREIGN KEY (CPF_Participante) REFERENCES Participante(CPF)
 );
 
 CREATE TABLE Certificado(
     Codigo INTEGER PRIMARY KEY,
     Cod_Inscricao INTEGER,
-    Tipo VARCHAR(30),
-    Emitido BOOLEAN,
-    DataEmissao DATE,
-    FOREIGN KEY (Cod_Inscricao) REFERENCES Inscricao(Codigo)
+	Tipo VARCHAR(30),
+	Emitido BOOLEAN,
+	DataEmissao DATE,
+	FOREIGN KEY (Cod_Inscricao) REFERENCES Inscricao(Codigo)
 );
 
 CREATE TABLE Feedback(
     Codigo INTEGER PRIMARY KEY,
     Cod_Inscricao INTEGER,
-    Nota INTEGER CHECK (Nota BETWEEN 1 AND 10),
-    Comentario TEXT,
-    FOREIGN KEY (Cod_Inscricao) REFERENCES Inscricao(Codigo)
+	Nota INTEGER CHECK (Nota BETWEEN 1 AND 10),
+	Comentario TEXT,
+	FOREIGN KEY (Cod_Inscricao) REFERENCES Inscricao(Codigo)
 );
 
 CREATE TABLE PerguntasFrequentes(
     Codigo INTEGER PRIMARY KEY,
     Pergunta TEXT,
-    Resposta TEXT
+	Resposta TEXT
 );
